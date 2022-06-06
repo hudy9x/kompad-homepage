e50fae5558a1b1fe32cee63564d3605fd1b6cbf0
hudy9x <hudy9x@gmail.com>
2022-06-06 17:53:14 +0700

    initial commit

--- release.js
+++
@@ -1,5 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-const releaseDate = path.join(__dirname, "release-date");
-
-fs.writeFileSync(releaseDate, new Date().getTime().toString(), "utf-8");
