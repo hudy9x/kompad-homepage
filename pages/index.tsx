15915235d5addde3e3df4126a606e5275374e071
hudy9x <95471659+hudy9x@users.noreply.github.com>
2022-06-07 11:02:31 +0700

    refactor: change favicon (#3)

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import LogoImage from "../assets/logo.png";
 import Link from "next/link";
 import { RiWindowsFill } from "react-icons/ri";
 import { IoLogoApple } from "react-icons/io";
+import Head from "next/head";
 
 interface IReleaseAsset {
   browser_download_url: string;
@@ -31,111 +32,116 @@ export default function Home({ release }: { release: IRelease }) {
   const macDownloadLink = macFile ? macFile.browser_download_url : "";
 
   return (
-    <div className="relative bg-white overflow-hidden">
-      <div
-        className="hidden lg:block lg:absolute lg:inset-0"
-        aria-hidden="true"
-      >
-        <svg
-          className="absolute top-0 left-1/2 transform translate-x-64 -translate-y-8"
-          width={640}
-          height={784}
-          fill="none"
-          viewBox="0 0 640 784"
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
+      <div className="relative bg-white overflow-hidden">
+        <div
+          className="hidden lg:block lg:absolute lg:inset-0"
+          aria-hidden="true"
         >
-          <defs>
-            <pattern
-              id="9ebea6f4-a1f5-4d96-8c4e-4c2abf658047"
-              x={118}
-              y={0}
-              width={20}
-              height={20}
-              patternUnits="userSpaceOnUse"
-            >
-              <rect
-                x={0}
-                y={0}
-                width={4}
-                height={4}
-                className="text-gray-200"
-                fill="currentColor"
-              />
-            </pattern>
-          </defs>
-          <rect
-            y={72}
+          <svg
+            className="absolute top-0 left-1/2 transform translate-x-64 -translate-y-8"
             width={640}
-            height={640}
-            className="text-gray-50"
-            fill="currentColor"
-          />
-          <rect
-            x={118}
-            width={404}
             height={784}
-            fill="url(#9ebea6f4-a1f5-4d96-8c4e-4c2abf658047)"
-          />
-        </svg>
-      </div>
+            fill="none"
+            viewBox="0 0 640 784"
+          >
+            <defs>
+              <pattern
+                id="9ebea6f4-a1f5-4d96-8c4e-4c2abf658047"
+                x={118}
+                y={0}
+                width={20}
+                height={20}
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x={0}
+                  y={0}
+                  width={4}
+                  height={4}
+                  className="text-gray-200"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              y={72}
+              width={640}
+              height={640}
+              className="text-gray-50"
+              fill="currentColor"
+            />
+            <rect
+              x={118}
+              width={404}
+              height={784}
+              fill="url(#9ebea6f4-a1f5-4d96-8c4e-4c2abf658047)"
+            />
+          </svg>
+        </div>
 
-      <div className="relative pt-6 pb-16 sm:pb-14 lg:pb-32">
-        <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-48">
-          <div className="lg:grid lg:grid-cols-12 lg:gap-8 items-center">
-            <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
-              <h1>
-                <span className="mt-1 block text-3xl tracking-tight font-extrabold sm:text-4xl sm:leading-normal xl:text-5xl xl:leading-normal">
-                  <span className="block text-gray-900">
-                    Kompad công cụ ghi chú
+        <div className="relative pt-6 pb-16 sm:pb-14 lg:pb-32">
+          <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-48">
+            <div className="lg:grid lg:grid-cols-12 lg:gap-8 items-center">
+              <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
+                <h1>
+                  <span className="mt-1 block text-3xl tracking-tight font-extrabold sm:text-4xl sm:leading-normal xl:text-5xl xl:leading-normal">
+                    <span className="block text-gray-900">
+                      Kompad công cụ ghi chú
+                    </span>
+                    <span className="block text-yellow-400">dành cho dev</span>
                   </span>
-                  <span className="block text-yellow-400">dành cho dev</span>
-                </span>
-              </h1>
-              <p className="mt-3 text-base leading-normal text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl sm:leading-9 xl:leading-9">
-                Một công cụ đơn giản để ghi chú và soạn thảo nội dung. Cung cấp
-                chỉ những tính năng thiết yếu cho bạn như: Tiêu đề, đánh số thứ
-                tự, hightlight nội dung hay tạo checklist đơn giản
-              </p>
-              <div className="mt-8 sm:12 sm:max-w-lg sm:mx-auto sm:text-center lg:text-left lg:mx-0">
-                <p className="text-base font-medium text-gray-900">
-                  📁 Tải xuống bộ cài tại đây
+                </h1>
+                <p className="mt-3 text-base leading-normal text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl sm:leading-9 xl:leading-9">
+                  Một công cụ đơn giản để ghi chú và soạn thảo nội dung. Cung
+                  cấp chỉ những tính năng thiết yếu cho bạn như: Tiêu đề, đánh
+                  số thứ tự, hightlight nội dung hay tạo checklist đơn giản
                 </p>
+                <div className="mt-8 sm:12 sm:max-w-lg sm:mx-auto sm:text-center lg:text-left lg:mx-0">
+                  <p className="text-base font-medium text-gray-900">
+                    📁 Tải xuống bộ cài tại đây
+                  </p>
 
-                <div className="mt-5 flex gap-4 sm:justify-center md:justify-start">
-                  <Link href={windowDownloadLink}>
-                    <button
-                      type="button"
-                      disabled={!windowDownloadLink}
-                      className={`inline-flex items-center gap-2 px-5 py-2 w-52 justify-center border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none`}
-                    >
-                      <RiWindowsFill className="-ml-2" />
-                      <span>Windows</span>
-                    </button>
-                  </Link>
-                  <Link href={macDownloadLink}>
-                    <button
-                      type="button"
-                      disabled={!macDownloadLink}
-                      className="disabled:opacity-70 inline-flex items-center gap-2 px-5 py-2 w-52 justify-center border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none"
-                    >
-                      <IoLogoApple className="-ml-2" />
-                      <span>Apple</span>
-                    </button>
-                  </Link>
+                  <div className="mt-5 flex gap-4 sm:justify-center md:justify-start">
+                    <Link href={windowDownloadLink}>
+                      <button
+                        type="button"
+                        disabled={!windowDownloadLink}
+                        className={`inline-flex items-center gap-2 px-5 py-2 w-52 justify-center border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none`}
+                      >
+                        <RiWindowsFill className="-ml-2" />
+                        <span>Windows</span>
+                      </button>
+                    </Link>
+                    <Link href={macDownloadLink}>
+                      <button
+                        type="button"
+                        disabled={!macDownloadLink}
+                        className="disabled:opacity-70 inline-flex items-center gap-2 px-5 py-2 w-52 justify-center border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none"
+                      >
+                        <IoLogoApple className="-ml-2" />
+                        <span>Apple</span>
+                      </button>
+                    </Link>
+                  </div>
+
+                  <p className="mt-5 text-sm text-gray-500">
+                    Lưu ý: Đây chỉ là phiên bản thử nghiệm nên toàn bộ dữ liệu
+                    sẽ bị xóa đi .
+                  </p>
                 </div>
-
-                <p className="mt-5 text-sm text-gray-500">
-                  Lưu ý: Đây chỉ là phiên bản thử nghiệm nên toàn bộ dữ liệu sẽ
-                  bị xóa đi .
-                </p>
+              </div>
+              <div className="mt-12 shadow-2xl relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+                <Image className="w-full -mt-6" src={AppImage} alt="" />
               </div>
             </div>
-            <div className="mt-12 shadow-2xl relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
-              <Image className="w-full -mt-6" src={AppImage} alt="" />
-            </div>
-          </div>
-        </main>
+          </main>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
