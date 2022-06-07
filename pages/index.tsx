fbe90d21b2c5449d95c22ced381b4317f364900e
hudy9x <hudy9x@gmail.com>
2022-06-07 10:47:39 +0700

    feat(home): add download button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,35 @@
-import { Fragment } from "react";
-import { Popover, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { GetStaticProps } from "next";
 import Image from "next/image";
 import AppImage from "../assets/app-image.png";
 import LogoImage from "../assets/logo.png";
+import Link from "next/link";
+import { RiWindowsFill } from "react-icons/ri";
+import { IoLogoApple } from "react-icons/io";
 
-const navigation = [
-  { name: "Product", href: "#" },
-  { name: "Features", href: "#" },
-  { name: "Marketplace", href: "#" },
-  { name: "Company", href: "#" },
-];
+interface IReleaseAsset {
+  browser_download_url: string;
+  size: number;
+  download_count: number;
+  name: string;
+}
 
-export default function Example() {
+interface IRelease {
+  all_releases: string;
+  author: string;
+  published_at: string;
+  release_notes: string;
+  releases: IReleaseAsset[];
+  tag_name: string;
+}
+
+export default function Home({ release }: { release: IRelease }) {
+  const { releases } = release;
+  const windowFile = releases.find((r) => /\.msi$/.test(r.name));
+  const macFile = releases.find((r) => /\.dmg$/.test(r.name));
+  const windowDownloadLink = windowFile ? windowFile.browser_download_url : "";
+  const macDownloadLink = macFile ? macFile.browser_download_url : "";
+
   return (
     <div className="relative bg-white overflow-hidden">
       <div
@@ -61,143 +78,59 @@ export default function Example() {
         </svg>
       </div>
 
-      <div className="relative pt-6 pb-16 sm:pb-24 lg:pb-32">
-        <Popover>
-          <nav
-            className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
-            aria-label="Global"
-          >
-            <div className="flex items-center flex-1">
-              <div className="flex items-center justify-between w-full md:w-auto">
-                <a href="#">
-                  <span className="sr-only">Workflow</span>
-                  <Image
-                    className="h-8 w-auto sm:h-10"
-                    width={50}
-                    height={50}
-                    objectFit={"contain"}
-                    src={LogoImage}
-                    alt=""
-                  />
-                </a>
-                {/* <div className="-mr-2 flex items-center md:hidden">
-                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                    <span className="sr-only">Open main menu</span>
-                    <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                  </Popover.Button>
-                </div> */}
-              </div>
-              {/* <div className="hidden md:block md:ml-10 md:space-x-10">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="font-medium text-gray-500 hover:text-gray-900"
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div> */}
-            </div>
-            <div className="hidden md:block text-right">
-              <span className="inline-flex rounded-md shadow-md ring-1 ring-black ring-opacity-5">
-                <a
-                  href="#"
-                  className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-yellow-600 bg-white hover:bg-gray-50"
-                >
-                  Log in
-                </a>
-              </span>
-            </div>
-          </nav>
-
-          {/* <Transition
-            as={Fragment}
-            enter="duration-150 ease-out"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="duration-100 ease-in"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
-          >
-            <Popover.Panel
-              focus
-              className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-            >
-              <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                <div className="px-5 pt-4 flex items-center justify-between">
-                  <div>
-                    <Image className="h-8 w-auto" src={LogoImage} alt="" />
-                  </div>
-                  <div className="-mr-2">
-                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                      <span className="sr-only">Close main menu</span>
-                      <XIcon className="h-6 w-6" aria-hidden="true" />
-                    </Popover.Button>
-                  </div>
-                </div>
-                <div className="px-2 pt-2 pb-3 space-y-1">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-                <a
-                  href="#"
-                  className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
-                >
-                  Log in
-                </a>
-              </div>
-            </Popover.Panel>
-          </Transition> */}
-        </Popover>
-
-        <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-32">
-          <div className="lg:grid lg:grid-cols-12 lg:gap-8">
+      <div className="relative pt-6 pb-16 sm:pb-14 lg:pb-32">
+        <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-48">
+          <div className="lg:grid lg:grid-cols-12 lg:gap-8 items-center">
             <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
               <h1>
-                <span className="mt-1 block text-4xl tracking-tight font-extrabold sm:text-5xl xl:text-6xl leading-normal">
+                <span className="mt-1 block text-3xl tracking-tight font-extrabold sm:text-4xl sm:leading-normal xl:text-5xl xl:leading-normal">
                   <span className="block text-gray-900">
-                    Kompad công cụ edit
+                    Kompad công cụ ghi chú
                   </span>
                   <span className="block text-yellow-400">dành cho dev</span>
                 </span>
               </h1>
-              <p className="mt-3 text-base leading-normal text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+              <p className="mt-3 text-base leading-normal text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl sm:leading-9 xl:leading-9">
                 Một công cụ đơn giản để ghi chú và soạn thảo nội dung. Cung cấp
                 chỉ những tính năng thiết yếu cho bạn như: Tiêu đề, đánh số thứ
                 tự, hightlight nội dung hay tạo checklist đơn giản
               </p>
-              <div className="mt-5 sm:max-w-lg sm:mx-auto sm:text-center lg:text-left lg:mx-0">
-                {/* <p className="text-base font-medium text-gray-900">
-                  Tải xuống bộ cài tại đây
-                </p> */}
-                <button
-                  type="button"
-                  className="mt-3 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-yellow-700 bg-yellow-100 hover:bg-yellow-200"
-                >
-                  Tải xuống bộ cài tại đây
-                </button>
+              <div className="mt-8 sm:12 sm:max-w-lg sm:mx-auto sm:text-center lg:text-left lg:mx-0">
+                <p className="text-base font-medium text-gray-900">
+                  📁 Tải xuống bộ cài tại đây
+                </p>
+
+                <div className="mt-5 flex gap-4 sm:justify-center">
+                  <Link href={windowDownloadLink}>
+                    <button
+                      type="button"
+                      disabled={!windowDownloadLink}
+                      className={`inline-flex items-center gap-2 px-5 py-2 w-52 justify-center border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none`}
+                    >
+                      <RiWindowsFill className="-ml-2" />
+                      <span>Windows</span>
+                    </button>
+                  </Link>
+                  <Link href={macDownloadLink}>
+                    <button
+                      type="button"
+                      disabled={!macDownloadLink}
+                      className="disabled:opacity-70 inline-flex items-center gap-2 px-5 py-2 w-52 justify-center border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none"
+                    >
+                      <IoLogoApple className="-ml-2" />
+                      <span>Apple</span>
+                    </button>
+                  </Link>
+                </div>
+
                 <p className="mt-5 text-sm text-gray-500">
                   Lưu ý: Đây chỉ là phiên bản thử nghiệm nên toàn bộ dữ liệu sẽ
-                  bị xóa đi
-                  {/* <a href="#" className="font-medium text-gray-900 underline">
-                    Privacy Policy
-                  </a> */}
-                  .
+                  bị xóa đi .
                 </p>
               </div>
             </div>
-            <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
-              <div className="relative mx-auto w-full lg:max-w-md">
-                <Image className="w-full" src={AppImage} alt="" />
-              </div>
+            <div className="mt-12 shadow-2xl relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+              <Image className="w-full -mt-6" src={AppImage} alt="" />
             </div>
           </div>
         </main>
@@ -205,3 +138,15 @@ export default function Example() {
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch("https://komersions.vercel.app/api/release/latest");
+  const release = await res.json();
+
+  return {
+    props: {
+      release,
+    },
+    revalidate: 120,
+  };
+};
