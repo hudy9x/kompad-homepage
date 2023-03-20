c78290ea4a1e0cca8196a052d4d2dcb4f1e3dc04
hudy9x <hudy9x@gmail.com>
2023-03-20 15:45:49 +0700

    adding app-id to header for verifying identity

--- pages/api/notify.ts
+++ pages/api/notify.ts
@@ -1,18 +1,24 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+const url = process.env.ADMIN_URL || ""
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const body = req.body;
   const title = `Kompad Payment - ${body.method}`
   const message = `${body.email} sent ${body.amount}☘️ for ${body.unit} month`
+  const adminUrl = (link: string) =>
+    `${url.replace(/\/*$/g, '')}/${link.replace(/^\/*/g, '')}`
 
-  fetch('https://admin.kompad.app/api/notification/send', {
+  fetch(adminUrl('api/notification/send'), {
     method: "POST",
     headers: {
-      "content-type": "application/json"
+      "content-type": "application/json",
+      "app-id": "LANDINGPAGE"
     },
     body: JSON.stringify({
       title: title,
-      body: message
+      body: message,
+      link: adminUrl(`/transactions`)
     })
   })
   return res.status(200).end()
