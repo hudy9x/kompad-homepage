ab0a4fb891a6dc803b60ec05ca9472fbcf6f4eef
hudy9x <hudy9x@gmail.com>
2023-03-30 11:11:26 +0700

    fix(notification): wait for all notification sent successfully

--- pages/api/notify.ts
+++ pages/api/notify.ts
@@ -8,27 +8,43 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const promises = [];
   const body = req.body;
   const title = `Kompad Payment - ${body.method}`;
   const message = `${body.email} sent ${body.amount}☘️ for ${body.unit} month`;
   const adminUrl = (link: string) =>
     `${url.replace(/\/*$/g, "")}/${link.replace(/^\/*/g, "")}`;
 
-  fetch(adminUrl("api/notification/send"), {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-      "app-id": appId,
-    },
-    body: JSON.stringify({
-      title: title,
-      body: message,
-      link: adminUrl(`/transactions`),
-    }),
-  });
+  promises.push(
+    fetch(adminUrl("api/notification/send"), {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "app-id": appId,
+      },
+      body: JSON.stringify({
+        title: title,
+        body: message,
+        link: adminUrl(`/transactions`),
+      }),
+    })
+  );
 
   // send notification via Discord
-  sendNotification(`${body.method === "PAYPAL" ? "🎁" : "💳"} ${message}`);
+  promises.push(
+    sendNotification(`${body.method === "PAYPAL" ? "🎁" : "💳"} ${message}`)
+  );
 
-  return res.status(200).end();
+  Promise.allSettled(promises)
+    .then((result) => {
+      console.log("sending notification success");
+    })
+    .catch((err) => {
+      console.log("sending notification failure");
+      console.log(err);
+    })
+    .finally(() => {
+      console.log("End");
+      res.status(500).end();
+    });
 }
