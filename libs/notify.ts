e4ed2ce1c7c16f5107348a5116325506a30e9155
hudy9x <hudy9x@gmail.com>
2023-03-30 13:49:49 +0700

    update webhook with content type

--- libs/notify.ts
+++ libs/notify.ts
@@ -2,11 +2,20 @@ import axios from "axios";
 
 export const sendNotification = (message: string) => {
   const url =
-    "https://discord.com/api/webhooks/1090874314041081886/1W7vyXkCxykobIDbBJX8e5gI_VYGdNOUeNH8id9ebsVxAxLth9-m2nAvWEkLPqVdnR7Y";
+    "https://discord.com/api/webhooks/1090890483108163584/fiXZlxaVic-2ear6beNAtYQVi8JCdylNVmzdRL9Wj7Moce1oOHq1EaCfC-1mm8OMgCDo";
 
-  return axios.post(url, {
-    content: message,
-    avatar_url: "https://www.kompad.app/favicon.png",
-    username: "Kompad",
-  });
+  console.log("start sending webhook discord");
+  return axios.post(
+    url,
+    {
+      content: message,
+      avatar_url: "https://www.kompad.app/favicon.png",
+      username: "Kompad",
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 };
