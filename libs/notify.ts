d40044174a89492901ce902e4e92675082af72de
hudy9x <hudy9x@gmail.com>
2023-03-30 14:20:03 +0700

    move webhook url to .env file

--- libs/notify.ts
+++ libs/notify.ts
@@ -1,13 +1,20 @@
 import axios from "axios";
 
 export const sendNotification = (message: string) => {
-  const url =
-    "https://discord.com/api/webhooks/1090894421702234132/4bMHtW0jPY7Z-nsSuJfoNEGo1gjNfOdbXExqJZOI5gqJCYvxRSAmCArsQJmd8e0B7Gk-";
+  const url = process.env.DISCORD_WEBHOOK_URL || "";
 
   console.log("start sending webhook discord");
-  return axios.post(url, {
-    content: message,
-    avatar_url: "https://www.kompad.app/favicon.png",
-    username: "Kompad",
-  });
+  return axios.post(
+    url,
+    {
+      content: message,
+      avatar_url: "https://www.kompad.app/favicon.png",
+      username: "Kompad",
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 };
