68d734e3da94e3ffd00b67672fc3386976a267d0
hudy9x <hudy9x@gmail.com>
2023-03-30 11:18:54 +0700

    refactor(notification): update discord's webhook

--- libs/notify.ts
+++ libs/notify.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const sendNotification = (message: string) => {
   const url =
-    "https://discord.com/api/webhooks/1090184851799416882/JDN0mnVqhuPdPCT4Ygn1nGP76avCDpMZQB9i1Ahe0iagVc77ZGzedFqEur96mtb9nacf";
+    "https://discord.com/api/webhooks/1090851878851579924/GvqG156Qgmo-jUFLvg3n_Nh-JMoGZfq2FLBIPcGyXJBiLUQzT1vPNxmNaLzSmym90l81 ";
 
   return axios.post(url, {
     content: message,
