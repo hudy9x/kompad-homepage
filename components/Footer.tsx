06ba1b21b12a27bb16ea8102bb134cbf71233fd1
hudy9x <95471659+hudy9x@users.noreply.github.com>
2024-06-03 17:36:40 +0700

    refactor: update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <div id="footer" className="py-[20px] border-t bg-gray-150 text-center">
       <div className="main-box  sm:w-[750px]">
         <p className="text-gray-500 text-sm">Made with ❤ by</p>
-        <p className="text-gray-900">@hudy9x and my friends</p>
+        <p className="text-gray-900">@hudy9x</p>
       </div>
     </div>
   );
